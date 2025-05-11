702e22a0896af5246c6cbaf9a11a68ec2e79ce08
Aji Anaz <aji.anaz@gmail.com>
2025-05-11 15:53:52 +0700

    feat : update cors allowed headers

--- src/plugin/cors.js
+++ src/plugin/cors.js
@@ -6,7 +6,7 @@ export default async function corsPlugin(fastify) {
     origin: '*', // Izinkan akses dari semua domain
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders:
-      'Content-Type,Authorization,Bearer,unixtime,gateway_key,x-api-key'
+      'Content-Type,Authorization,Bearer,unixtime,gateway_key,x-api-key,api_access_token'
   }
   // Daftarkan plugin CORS
   await fastify.register(fastifyCors, corsOptions)
